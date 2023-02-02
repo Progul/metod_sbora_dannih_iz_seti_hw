31bb77897b62a9cc0b9517fdc98ba38843996ad6
Alexander <desnenko@ya.ru>
2023-02-02 19:22:19 +0400

    добавил 1 домашку

---
+++ HW_1/HW_1.py
@@ -0,0 +1,38 @@
+# 1. Посмотреть документацию к API GitHub, разобраться как вывести
+# список репозиториев для конкретного пользователя, сохранить
+# JSON-вывод в файле *.json.
+
+import requests
+import json
+from pprint import pprint
+
+path = 'https://api.github.com/users/progul/repos'
+
+response = requests.get(path)
+j_data = response.json()
+repos = [j_data[i]['name'] for i in range(len(j_data))]
+print(repos)
+
+with open('repos.json', 'w', encoding='UTF-8') as f:
+    json.dump(repos, f)
+
+# 2. Изучить список открытых API
+# (https://www.programmableweb.com/category/all/apis). Найти среди
+# них любое, требующее авторизацию (любого типа). Выполнить запросы к
+# нему, пройдя авторизацию. Ответ сервера записать в файл.
+# Если нет желания заморачиваться с поиском, возьмите API вконтакте
+# (https://vk.com/dev/first_guide). Сделайте запрос, чтобы получить
+# список всех сообществ на которые вы подписаны.
+
+path_flickr = f'https://www.flickr.com/services/rest/?method=flickr.' \
+         f'galleries.getPhotos&api_key=c473e4cfab72f334511de9ad' \
+         f'4f9f09c6&gallery_id=66911286-72157647277042064&forma' \
+         f't=json&nojsoncallback=1'
+
+resp_flickr = requests.get(path_flickr)
+j_flickr = resp_flickr.json()
+print(resp_flickr.text)
+pprint(j_flickr)
+
+with open('flickr.json', 'w', encoding='UTF-8') as f:
+    json.dump(j_flickr, f)
---
+++ lection/main.py
@@ -0,0 +1,6 @@
+import requests
+
+url = 'https://www.google.ru'
+
+responce = requests.get(url)
+
